6f1cada3099059ffb8f200c26c6baebd929bdd43
Bosegluon <ygcec@foxmail.com>
2025-08-03 21:27:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    url="https://github.com/Bosegluon2/maxtorch",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
